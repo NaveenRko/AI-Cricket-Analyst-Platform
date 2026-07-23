eea1d998d2be8d59bba37aa726999839223630e5
Naveen <ranapangunaveen9@gmail.com>
2026-07-23 20:11:09 +0530

    added memory window

--- memory/memory.py
+++ memory/memory.py
@@ -1,5 +1,9 @@
-from langchain_classic.memory import ConversationBufferMemory
+# from langchain_classic.memory import ConversationBufferMemory
 
-memory = ConversationBufferMemory(
-    return_messages=True
-)
+# memory = ConversationBufferMemory(
+#     return_messages=True
+# )
+
+from langchain.memory import ConversationBufferWindowMemory
+
+memory = ConversationBufferWindowMemory(k=4, return_messages=True)
